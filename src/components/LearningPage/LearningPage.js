66573a869f1b7452c2c69399b492680acdff9851
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-08-07 11:55:38 -0400

    working on learning page

---
+++ src/components/LearningPage/LearningPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { Input, Label } from '../Form/Form';
+import Button from '../Button/Button';
+import UserContext from '../../contexts/UserContext';
+
+export default class LearningPage extends Component {
+  static contextType = UserContext
+
+}
